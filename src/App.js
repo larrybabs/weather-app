38d8a44ba5517ee2a8de7ac1df436dae83bc95ce
Azeez Samad <seshp@ymail.com>
2022-05-04 11:41:15 +0100

    add weather api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@
 import './App.css';
 
 function App() {
+
+  const api = {
+    key: "41d99c6b20c1c612be72e055ec8f6e94",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
   return (
     <div className="App">
       
